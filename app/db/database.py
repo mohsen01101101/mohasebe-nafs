7c69710d2efd55648cdf02ef7332dbf3fd3a39b8
mohsen01101101 <mohsenmn@proton.me>
2026-06-30 09:13:27 +0330

    refactor(database): change sqlite foreign_keys PRAGMA from Engine-level to instance-level

--- app/db/database.py
+++ app/db/database.py
@@ -1,12 +1,12 @@
 from sqlmodel import create_engine, Session
 from app.core.config import settings
-from sqlalchemy import event, Engine
+from sqlalchemy import event
 
 
 engine = create_engine(settings.sqlite_url)
 
 
-@event.listens_for(Engine, "connect")
+@event.listens_for(engine, "connect")
 def set_sqlite_pragma(dbapi_connection, _):
     cursor = dbapi_connection.cursor()
     cursor.execute("PRAGMA foreign_keys=ON")
