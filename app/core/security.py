5b075ac185c013c9c012dff466c7c11b51b1619a
mohsen01101101 <mohsenmn@proton.me>
2026-07-07 18:31:48 +0330

    add JWT access token creation and decoding

--- app/core/security.py
+++ app/core/security.py
@@ -1,4 +1,8 @@
 import bcrypt
+import jwt
+from datetime import datetime, timedelta
+from app.core.constants import IRAN_TZ
+from app.core.config import settings
 
 
 def hash_password(password: str) -> str:
@@ -13,3 +17,39 @@ def verify_password(password: str, password_hash: str) -> bool:
         password.encode("utf-8"),
         password_hash.encode("utf-8")
     )
+
+
+def create_access_token(user_id: int):
+    expire = datetime.now(IRAN_TZ) + timedelta(
+        minutes=settings.access_token_expire_minutes
+    )
+
+    payload = {
+        "sub": str(user_id),
+        "exp": expire
+    }
+
+    token = jwt.encode(
+        payload=payload,
+        key=settings.secret_key,
+        algorithm=settings.algorithm
+    )
+
+    return token
+
+
+def decode_access_token(token: str):
+    try:
+        payload = jwt.decode(
+            jwt=token,
+            key=settings.secret_key,
+            algorithms=[settings.algorithm]
+        )
+
+        return payload
+
+    except jwt.ExpiredSignatureError:
+        raise ValueError("Token has expired.")
+
+    except jwt.InvalidTokenError:
+        raise ValueError("Invalid token.")
