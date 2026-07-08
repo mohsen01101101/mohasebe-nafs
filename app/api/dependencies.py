cf3b15c6f5cd6885e6766e4ae38abb18622a0419
mohsen01101101 <mohsenmn@proton.me>
2026-07-08 14:36:33 +0330

    add get_current_user to api dependencies

--- app/api/dependencies.py
+++ app/api/dependencies.py
@@ -1,10 +1,35 @@
-from fastapi import Depends
+from fastapi import Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
 from sqlmodel import Session
 from app.db.database import get_session
 from app.services.user import UserService
+from app.core.security import decode_access_token
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/users/login")
 
 
 def get_user_service(
     session: Session = Depends(get_session)
 ):
     return UserService(session)
+
+
+def get_current_user(
+    token: str = Depends(oauth2_scheme),
+    service: UserService = Depends(get_user_service)
+):
+    try:
+        payload = decode_access_token(token)
+
+        user_id = int(payload["sub"])
+
+        user = service.get_by_id(user_id)
+
+        return user
+
+    except (ValueError, KeyError, TypeError) as e:
+        raise HTTPException(
+            status_code=401,
+            detail=str(e)
+        )
