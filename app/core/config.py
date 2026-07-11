5108020d87b735850053ae7db691d68626818bf6
mohsen01101101 <mohsenmn@proton.me>
2026-07-11 17:15:16 +0330

    feat(config): add api url, prefix and base url property

--- app/core/config.py
+++ app/core/config.py
@@ -7,13 +7,21 @@ class Settings(BaseSettings):
         extra="ignore",
     )
 
+    # Database
     sqlite_url: str
 
+    # Auth
     secret_key: str
-
     algorithm: str = "HS256"
-
     access_token_expire_minutes: int = 60
+
+    # API
+    api_url: str
+    api_prefix: str = "/api"
+
+    @property
+    def api_base_url(self):
+        return f"{self.api_url}{self.api_prefix}"
 
 
 settings = Settings()  # type: ignore[call-arg]
