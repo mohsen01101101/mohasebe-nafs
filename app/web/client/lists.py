2a9d1cd3850950dc7fe2bb07bce09bd5cb9ee724
mohsen01101101 <mohsenmn@proton.me>
2026-07-12 13:33:26 +0330

    implement list api client

---
+++ app/web/client/lists.py
@@ -0,0 +1,88 @@
+from app.core.config import settings
+from app.web.client.http import client
+from datetime import datetime
+
+
+BASE_URL = f"{settings.api_base_url}/users"
+
+
+def get_lists(token: str, user_id: int):
+    response = client.get(
+        url=f"{BASE_URL}/{user_id}/lists",
+        headers={
+            "Authorization": f"Bearer {token}"
+        }
+    )
+
+    response.raise_for_status()
+
+    return response.json()
+
+
+def get_my_lists(token: str):
+    response = client.get(
+        url=f"{BASE_URL}/me/lists",
+        headers={
+            "Authorization": f"Bearer {token}"
+        }
+    )
+
+    response.raise_for_status()
+
+    return response.json()
+
+
+def create_list(
+    token: str,
+    title: str,
+    created_at: datetime | None = None
+):
+    data = {
+        "title": title
+    }
+
+    if created_at is not None:
+        data["created_at"] = created_at.isoformat()
+
+    response = client.post(
+        url=f"{BASE_URL}/me/lists",
+        headers={
+            "Authorization": f"Bearer {token}"
+        },
+        json=data
+    )
+
+    response.raise_for_status()
+
+    return response.json()
+
+
+def update_list(
+        token: str,
+        list_id: int,
+        title: str
+):
+    response = client.patch(
+        url=f"{BASE_URL}/me/{list_id}",
+        headers={
+            "Authorization": f"Bearer {token}"
+        },
+        json={
+            "title": title
+        }
+    )
+
+    response.raise_for_status()
+
+    return response.json()
+
+
+def delete_list(token: str, list_id: int):
+    response = client.delete(
+        url=f"{BASE_URL}/me/{list_id}",
+        headers={
+            "Authorization": f"Bearer {token}"
+        },
+    )
+
+    response.raise_for_status()
