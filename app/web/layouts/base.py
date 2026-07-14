b5c6115ee38f0ac88ff825bbe980f385dc5d77d2
mohsen01101101 <mohsenmn@proton.me>
2026-07-14 19:41:34 +0330

    add separate layouts for auth and app pages

--- app/web/layouts/base.py
+++ app/web/layouts/base.py
@@ -3,7 +3,7 @@ from app.web.components.header import header
 from app.web.components.footer import footer
 
 
-def base_layout(page_content, title="Mohasebe Nafs"):
+def base_layout(page_content, title="Mohasebe Nafs", show_header_footer=True):
     return Html(
         Head(
             Meta(charset="UTF-8"),
@@ -19,16 +19,32 @@ def base_layout(page_content, title="Mohasebe Nafs"):
         ),
 
         Body(
-            Header(
-                header()
+            *(
+                [Header(header())] if show_header_footer else []
             ),
             Main(
                 page_content
             ),
-            Footer(
-                footer()
+            *(
+                [Footer(footer())] if show_header_footer else []
             )
         ),
 
         lang="fa"
+    )
+
+
+def auth_layout(page_content, title="Login"):
+    return base_layout(
+        page_content=page_content,
+        title=title,
+        show_header_footer=False
+    )
+
+
+def app_layout(page_content, title="Mohasebe Nafs"):
+    return base_layout(
+        page_content=page_content,
+        title=title,
+        show_header_footer=True
     )
