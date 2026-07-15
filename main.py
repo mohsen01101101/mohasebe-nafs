1a3dd14081f91e8d183b62fdf7c27db9037f2799
mohsen01101101 <mohsenmn@proton.me>
2026-07-15 16:45:24 +0330

    feat: update fasthtml main.py to mount API app

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from fasthtml.common import fast_app, serve
+from app.api.main import api_app
+from app.core.config import settings
 from app.web.pages.home import home
 
 
 app, rt = fast_app(
     pico=False,
     static_path="static"
+)
+
+
+app.mount(
+    path=settings.api_prefix,
+    app=api_app
 )
 
 
