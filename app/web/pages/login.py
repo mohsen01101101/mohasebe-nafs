7628c4a6c8393f00843ae2edeffbd8240f4a8026
mohsen01101101 <mohsenmn@proton.me>
2026-07-16 18:10:21 +0330

    feat: implement login page

---
+++ app/web/pages/login.py
@@ -0,0 +1,76 @@
+from fasthtml.common import *
+from fasthtml.svg import *
+from app.web.layouts.base import auth_layout
+
+
+def login():
+    page_content = Div(
+        Form(
+            Label(
+                Span(
+                    "شماره همراه",
+                    cls="label"
+                ),
+
+                Input(
+                    name="phone_number",
+                    type="tel",
+                    required=True,
+                    placeholder="شماره همراه",
+                    pattern="[0-9]*",
+                    minlength="11",
+                    maxlength="11",
+                    title="Phone number must be 11 digits",
+                    cls="input validator tabular-nums",
+                    dir="rtl"
+                ),
+
+                Span(
+                    "شماره همراه باید ۱۱ رقم باشد",
+                    cls="validator-hint hidden"
+                ),
+
+                cls="fieldset"
+            ),
+
+            Label(
+                Span(
+                    "رمز عبور",
+                    cls="label"
+                ),
+
+                Input(
+                    name="password",
+                    type="password",
+                    required=True,
+                    placeholder="رمز عبور",
+                    minlength="8",
+                    cls="input validator"
+                ),
+
+                Span(
+                    "رمز عبور باید حداقل 8 کاراکتر باشد",
+                    cls="validator-hint hidden"
+                ),
+
+                cls="fieldset"
+            ),
+
+            Button(
+                "ورود",
+                type="submit",
+                cls="btn btn-primary mt-4"
+            ),
+
+            action="/login",
+            method="post",
+            cls="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4"
+        ),
+
+        cls="flex items-center h-dvh"
+    )
+
+    return auth_layout(
+        page_content=page_content,
+        title="صفحه ورود"
+    )
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from fasthtml.common import fast_app, serve
+from fasthtml.common import fast_app, serve, Redirect
 from app.api.main import api_app
 from app.core.config import settings
 from app.web.pages.home import home
+from app.web.pages.login import login
+from app.web.client.auth import login as client_login
 
 
 app, rt = fast_app(
@@ -14,6 +16,21 @@ app.mount(
     path=settings.api_prefix,
     app=api_app
 )
+
+
+@rt("/login")
+def get():  # pyright: ignore[reportRedeclaration]
+    return login()
+
+
+@rt("/login")
+def post(phone_number: str, password: str):
+    data = client_login(
+        phone_number=phone_number,
+        password=password
+    )
+
+    return Redirect("/")
 
 
 @rt("/")
