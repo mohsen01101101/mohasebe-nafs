8c4dd9b35c3906c6b867a69e92676db4aee0e4dd
mohsen01101101 <mohsenmn@proton.me>
2026-07-22 16:07:33 +0330

    feat: display lists on home page

--- app/web/pages/home.py
+++ app/web/pages/home.py
@@ -1,9 +1,30 @@
 from fasthtml.common import *
+from datetime import date
 from app.web.layouts.base import app_layout
+from app.web.client.lists import get_my_lists
+from app.web.components.datepicker import datepicker
+from app.web.components.lists import lists
 
 
 def home(req: Request):
-    page_content = P("محتوای صفحه")
+    token = req.session["access_token"]
+
+    lists_data = get_my_lists(
+        token=token,
+        selected_date=date.today()
+    )
+    lists_html = lists(lists_data)
+
+    page_content = (
+        H1(
+            "اعمال امروز",
+            cls="text-center text-2xl font-bold mb-4"
+        ),
+
+        datepicker(),
+
+        lists_html
+    )
 
     return app_layout(
         req=req,
