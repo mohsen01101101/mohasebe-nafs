d5de269cdea27b644f8fdfae90311fa0cfd5962d
mohsen01101101 <mohsenmn@proton.me>
2026-07-23 23:41:40 +0330

    feat: update actions handler to support daily state

--- app/web/handlers/actions.py
+++ app/web/handlers/actions.py
@@ -1,12 +1,12 @@
 from fasthtml.common import *
-from app.web.client.actions import get_my_actions
+from app.web.client.actions import get_my_actions_with_state
 from app.core.utils.date_converter import jalali_to_gregorian
 from app.web.components.actions import actions
 
 
 def register_action_routes(rt):
     @rt("/web-api/lists/{list_id}/actions")
-    def get_actions(
+    def get_actions_with_state(
         session,
         list_id: int,
         jalali_date: str | None = None
@@ -18,11 +18,11 @@ def register_action_routes(rt):
         if jalali_date:
             selected_date = jalali_to_gregorian(jalali_date)
 
-        actions_data = get_my_actions(
+        actions_with_state_data = get_my_actions_with_state(
             token=token,
             list_id=list_id,
             selected_date=selected_date
         )
-        actions_html = actions(actions_data)
+        actions_with_state_html = actions(actions_with_state_data)
 
-        return actions_html
+        return actions_with_state_html
